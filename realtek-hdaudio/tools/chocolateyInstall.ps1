014067fe550d198b2002251648c39fa165492cfb
pocki <pock_christoph@gmx.net>
2016-02-14 10:23:10 +0100

    Added: realtek-hdaudio and via-hdaudio

---
+++ realtek-hdaudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'realtek-hdaudio'
+$version = 'R279'
+$installerType = 'exe'
+$url = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0006-32bit_Win7_Win8_Win81_Win10_$version.exe"
+$url64 = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0006-64bit_Win7_Win8_Win81_Win10_$version.exe"
+$silentArgs = '/S /norestart'
+$validExitCodes = @(0,3010);  # http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes
---
+++ via-hdaudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'via-hdaudio';
+$version = 'v11_1100e';
+$fileType = 'exe';
+$silentArgs = '-s -noreboot';
+
+$WindowsVersion = (Get-WmiObject -Class Win32_OperatingSystem).Caption
+if ( $WindowsVersion -match 'Windows 10' ) {
+	$url = "https://d34vhvz8ul1ifj.cloudfront.net/Driver/VIA_HD_Aud_v11_1100e_01282016.zip"
+}
+else {
+	$url = "https://d34vhvz8ul1ifj.cloudfront.net/Driver/v10_1200a.zip"
+}
+
+$unpackDir = "${ENV:TEMP}\via-hdaudio"
+New-Item "$unpackDir" -ItemType Directory -Force
+$unpackFile = "$unpackDir\via-hdaudio.zip"
+Get-ChocolateyWebFile $packageName $unpackFile $url
+Get-ChocolateyUnzip $unpackFile $unpackDir
+$file = "$unpackDir\VIAHDAud_v11_1100e_01282016\setup.exe"
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file 
+Remove-Item $unpackDir -Recurse -Force
