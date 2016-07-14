fa2b83be30e4c35d925ee5da8217715de1070819
pocki <pock_christoph@gmx.net>
2016-07-14 20:20:24 +0200

    Removed server OS block - is working on server OS

--- pdf24/tools/chocolateyInstall.ps1
+++ pdf24/tools/chocolateyInstall.ps1
@@ -1,15 +1,9 @@
 ﻿$packageName = 'pdf24'
 $installerType = 'MSI'
 $url = 'http://stx.pdf24.org/products/pdf-creator/download/pdf24-creator-7.9.0.msi'
-$checksum = '4199fef760e516b1656ff9aae1bfcb0c9dc44709';
+$checksum = '26b54c770591412c4515fe0cccea9a8cdfdc357f';
 $checksumType = 'sha1';
 $silentArgs = 'DESKTOPICONS=No FAXPRINTER=No /qn';
 $validExitCodes = @(0)
 
-if((Get-WmiObject Win32_OperatingSystem).ProductType -ne 1) {
-	throw "$packageName not supported on Windows Server.";
-	return;
-}
-else {
-	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType
-}
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType
