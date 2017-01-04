ff6add59b68455a2ee00a9f566f175b2adb3b06b
pocki <pock_christoph@gmx.net>
2017-01-04 19:17:16 +0100

    Updated logitechgaming v8.90.117

--- logitechgaming/tools/chocolateyInstall.ps1
+++ logitechgaming/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'logitechgaming'
 $installerType = 'EXE'
-$url = "https://www.logitech.com/pub/techsupport/gaming/LGS_8.89.68_x86_Logitech.exe"
-$url64 = "https://www.logitech.com/pub/techsupport/gaming/LGS_8.89.68_x64_Logitech.exe"
+$url = "https://www.logitech.com/pub/techsupport/gaming/LGS_8.90.117_x86_Logitech.exe"
+$url64 = "https://www.logitech.com/pub/techsupport/gaming/LGS_8.90.117_x64_Logitech.exe"
 $silentArgs = '/S'
-$checksum = '2536916800cef0a82adec9baa7205cac5ad884402b33bc13a5610653d496d811'
-$checksum64 = 'f087e9caf89183463d9ae62e6d31aae621f54da7c2d4faf720e65e31b3b0c058'
+$checksum = '4914e508f182714504b7683462b1be8328c0a123bd0998a668f5fb7fd9942d35'
+$checksum64 = '1039b00c8af3059301832add78963414fba359a606b3677533122059e30a3467'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -Checksum "$checksum" -ChecksumType "$checksumType" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType" -validExitCodes $validExitCodes
