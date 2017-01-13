4ac6140ec585c0752d5aa5658ad6715bdb5dd1f7
pocki <pock_christoph@gmx.net>
2017-01-13 14:58:45 +0100

    Updated Realtek HD Audio driver to R2.81

--- realtek-hdaudio/tools/chocolateyInstall.ps1
+++ realtek-hdaudio/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'realtek-hdaudio'
-$version = 'R280'
+$version = 'R281'
 $installerType = 'exe'
-$url = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0007-32bit_Win7_Win8_Win81_Win10_$version.exe"
-$url64 = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0007-64bit_Win7_Win8_Win81_Win10_$version.exe"
+$url = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0008-32bit_Win7_Win8_Win81_Win10_$version.exe"
+$url64 = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0008-64bit_Win7_Win8_Win81_Win10_$version.exe"
 $silentArgs = '/S /norestart'
 $validExitCodes = @(0,3010);  # http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
