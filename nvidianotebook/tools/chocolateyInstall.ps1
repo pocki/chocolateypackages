2c4ffcabe65ae2e5277ddf8c15a6e999901eb54f
Christoph Pock <christoph.pock@tugraz.at>
2017-03-20 20:35:17 +0100

    Updated nvidia drivers to version 378.92

--- nvidianotebook/tools/chocolateyInstall.ps1
+++ nvidianotebook/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'nvidianotebook'
-$version = '378.78'
+$version = '378.92'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot -nogfexp /passive /nosplash /noeula'
 $unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
