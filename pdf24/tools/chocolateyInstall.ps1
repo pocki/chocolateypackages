1368d18f16a9caf0bacd73786b332ca15dded8d0
Christoph Pock <christoph.pock@tugraz.at>
2017-07-05 20:36:40 +0200

    Updated pdf24 to v 8.2.1

--- pdf24/tools/chocolateyInstall.ps1
+++ pdf24/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'pdf24'
 $installerType = 'MSI'
-$url = 'http://stx.pdf24.org/products/pdf-creator/download/pdf24-creator-8.2.0.msi'
-$checksum = '304677f04927bd6e98e8a18f32c8f4a1bddecec3a443d0d09d25b887ab2702d3';
+$url = 'http://stx.pdf24.org/products/pdf-creator/download/pdf24-creator-8.2.1.msi'
+$checksum = 'f48d6abb91377c91e854a5122f834b1033eea9f05a1292cf8991641d834853f1';
 $checksumType = 'sha256';
 $silentArgs = 'DESKTOPICONS=No FAXPRINTER=No /qn';
 $validExitCodes = @(0)
