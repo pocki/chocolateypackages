85f63733cfa4ddb48a376331d0faa5b5fa5307d3
Christoph Pock <christoph.pock@tugraz.at>
2017-11-30 18:31:42 +0100

    Updated nvidia drivers to v388.43

--- nvidiadesktop/tools/chocolateyInstall.ps1
+++ nvidiadesktop/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'nvidiadesktop'
-$version = '388.31'
+$version = '388.43'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot -nogfexp /passive /nosplash /noeula'
 $unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
--- nvidianotebook/tools/chocolateyInstall.ps1
+++ nvidianotebook/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'nvidianotebook'
-$version = '388.31'
+$version = '388.43'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot -nogfexp /passive /nosplash /noeula'
 $unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
