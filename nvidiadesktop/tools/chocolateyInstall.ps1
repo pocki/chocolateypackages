3ca2eda1c516055368727549aeecd842323c0bee
Christoph Pock <christoph.pock@tugraz.at>
2017-12-15 20:17:30 +0100

    Updated nvidia drivers to v388.59

--- nvidiadesktop/tools/chocolateyInstall.ps1
+++ nvidiadesktop/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'nvidiadesktop'
-$version = '388.43'
+$version = '388.59'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot -nogfexp /passive /nosplash /noeula'
 $unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
