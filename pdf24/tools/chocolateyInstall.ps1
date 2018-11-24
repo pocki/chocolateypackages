88ca12574a756b6d87c70250ebe1b0db048c64f4
Christoph Pock <christoph.pock@tugraz.at>
2018-11-24 11:27:26 +0100

    Updated to v8.7.0, added teknowledgist

--- pdf24/tools/chocolateyInstall.ps1
+++ pdf24/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'pdf24'
 $installerType = 'MSI'
-$url = 'https://stx.pdf24.org/products/pdf-creator/download/pdf24-creator-8.6.1.msi'
-$checksum = '008c1441d1ffcb57636107fd8aa660c79b1e14044988593c64808d121db18b16';
+$url = 'https://stx.pdf24.org/products/pdf-creator/download/pdf24-creator-8.7.0.msi'
+$checksum = 'fd6b3c0908b7d8a6903c3566b5240ea87104bfd1914e03b7b9be1b2eb33f0988';
 $checksumType = 'sha256';
 $silentArgs = 'DESKTOPICONS=No FAXPRINTER=No /qn';
-$validExitCodes = @(0)
+$validExitCodes = @(0,3010)
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType
